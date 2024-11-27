c6757452ca46f1515fc4f07b5d1ea48717570be9
Sergio20100 <sergio20100@hotmail.com>
2024-11-26 22:29:59 -0500

    Avances de la Entrega 2 implementando websocket

---
+++ proyecto_preentrega_2/src/routes/products.router.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+// import uploader from "../utils/uploader.js";
+import ProductManager from "../managers/ProductManager.js";
+const router = Router();
+
+const productManager = new ProductManager();
+
+router.get("/", async (req, res) => {
+    try {
+        const products = await productManager.getAll();
+        res.status(200).json({ status: "success", payload:products });
+    } catch (error) {
+        res.status(error.code || 500).json({status:"error", message: error.message})
+    }
+});
+
+router.get("/:pid", async (req, res) => {
+    try {
+        const product = await productManager.getOneById(req.params?.pid);
+        res.status(200).json({ status: "success", payload:product });
+    } catch (error) {
+        res.status(error.code || 500).json({status:"error", message: error.message})
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        // console.log(req.body)
+        const product = await productManager.insertOne(req.body);
+        res.status(201).json({ status: "success", payload: product });
+    } catch (error) {
+        res.status(error.code || 500).json({status:"error", message: error.message})
+    }
+});
+
+router.put("/:pid", async (req, res) => {
+    try {
+        // console.log(req.params);
+        
+        const product = await productManager.updateOneById(req.params?.pid, req.body);
+        res.status(200).json({ status: "success", payload: product });
+    } catch (error) {
+        res.status(error.code || 500).json({status:"error", message: error.message})
+    }
+});
+
+router.delete("/:pid", async (req, res) => {
+    try {
+        const product = await productManager.deleteOneById(req.params?.pid);
+        res.status(200).json({ status: "success", payload: product });
+    } catch (error) {
+        res.status(error.code || 500).json({status:"error", message: error.message})
+    }
+});
+
+
+export default router;
